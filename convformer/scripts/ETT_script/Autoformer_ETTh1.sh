08241ef988e4fd19d4245837188a08d1e02bf895
Nikita <nkt.ochkin@gmail.com>
2025-06-02 14:47:53 +0300

    added scripts + plots

---
+++ convformer/scripts/ETT_script/Autoformer_ETTh1.sh
@@ -0,0 +1,25 @@
+# pyenv activate science-venv
+# cd Documents/VScodeProjects/RNN-TimeSeries-coursework/convformer
+# bash scripts/ETT_script/Autoformer_ETTh1.sh
+
+export CUDA_VISIBLE_DEVICES=0
+
+python -u run.py \
+  --is_training 1 \
+  --root_path ./data/raw/ETT-small/ \
+  --data_path ETTh1.csv \
+  --model_id ETTh1_96_24 \
+  --model Autoformer \
+  --data ETTh1 \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 24 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 3 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 1
