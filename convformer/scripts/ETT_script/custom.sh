bd7a838ba5f5c254ada73a1d9fb5b10c5f129845
Nikita <nkt.ochkin@gmail.com>
2025-06-08 12:35:15 +0300

    got results for pure convstem

---
+++ convformer/scripts/ETT_script/custom.sh
@@ -0,0 +1,200 @@
+# pyenv activate science-venv && cd Documents/VScodeProjects/RNN-TimeSeries-coursework/convformer && bash scripts/ETT_script/custom.sh
+
+export CUDA_VISIBLE_DEVICES=0
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# ConvStem # ConvStem # ConvStem # ConvStem # ConvStem # ConvStem # ConvStem # ConvStem # ConvStem  #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# 24
+python -u run.py \
+  --is_training 1 \
+  --root_path ./data/raw/ETT-small/ \
+  --data_path ETTh1.csv \
+  --model_id ETTh1_96_24 \
+  --model Informer_ConvStem \
+  --data ETTh1 \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 24 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 5 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 10
+
+# 48
+python -u run.py \
+  --is_training 1 \
+  --root_path ./data/raw/ETT-small/ \
+  --data_path ETTh1.csv \
+  --model_id ETTh1_96_48 \
+  --model Informer_ConvStem \
+  --data ETTh1 \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 48 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 5 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 10
+
+# 168
+python -u run.py \
+  --is_training 1 \
+  --root_path ./data/raw/ETT-small/ \
+  --data_path ETTh1.csv \
+  --model_id ETTh1_96_168 \
+  --model Informer_ConvStem \
+  --data ETTh1 \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 168 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 5 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 10
+
+# 336
+python -u run.py \
+  --is_training 1 \
+  --root_path ./data/raw/ETT-small/ \
+  --data_path ETTh1.csv \
+  --model_id ETTh1_96_336 \
+  --model Informer_ConvStem \
+  --data ETTh1 \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 336 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 5 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 10
+
+# 720
+python -u run.py \
+  --is_training 1 \
+  --root_path ./data/raw/ETT-small/ \
+  --data_path ETTh1.csv \
+  --model_id ETTh1_96_720 \
+  --model Informer_ConvStem \
+  --data ETTh1 \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 5 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 10
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Informer # Informer # Informer # Informer # Informer # Informer # Informer # Informer # Informer  #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# 48
+python -u run.py \
+  --is_training 1 \
+  --root_path ./data/raw/ETT-small/ \
+  --data_path ETTh1.csv \
+  --model_id ETTh1_96_48 \
+  --model Informer \
+  --data ETTh1 \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 48 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 5 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 10
+
+# 168
+python -u run.py \
+  --is_training 1 \
+  --root_path ./data/raw/ETT-small/ \
+  --data_path ETTh1.csv \
+  --model_id ETTh1_96_168 \
+  --model Informer \
+  --data ETTh1 \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 168 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 5 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 10
+
+# 336
+python -u run.py \
+  --is_training 1 \
+  --root_path ./data/raw/ETT-small/ \
+  --data_path ETTh1.csv \
+  --model_id ETTh1_96_336 \
+  --model Informer \
+  --data ETTh1 \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 336 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 5 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 10
+
+# 720
+python -u run.py \
+  --is_training 1 \
+  --root_path ./data/raw/ETT-small/ \
+  --data_path ETTh1.csv \
+  --model_id ETTh1_96_720 \
+  --model Informer \
+  --data ETTh1 \
+  --features M \
+  --seq_len 96 \
+  --label_len 48 \
+  --pred_len 720 \
+  --e_layers 2 \
+  --d_layers 1 \
+  --factor 5 \
+  --enc_in 7 \
+  --dec_in 7 \
+  --c_out 7 \
+  --des 'Exp' \
+  --itr 10
