ba8214084262d18d10350c468d000e90c2479de9
Nikita <nkt.ochkin@gmail.com>
2025-06-10 23:49:55 +0300

    coursework done

--- convformer/scripts/ETT_script/custom2.sh
+++ convformer/scripts/ETT_script/custom2.sh
@@ -3,26 +3,26 @@
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 export CUDA_VISIBLE_DEVICES=0
 
-# 720
-python -u run.py \
-  --is_training 1 \
-  --root_path ./data/raw/ETT-small/ \
-  --data_path ETTh1.csv \
-  --model_id ETTh1_96_720 \
-  --model Reformer \
-  --data ETTh1 \
-  --features M \
-  --seq_len 96 \
-  --label_len 48 \
-  --pred_len 720 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --enc_in 7 \
-  --dec_in 7 \
-  --c_out 7 \
-  --des 'Exp' \
-  --itr 1 \
-  --batch_size 30
+# # 720
+# python -u run.py \
+#   --is_training 1 \
+#   --root_path ./data/raw/ETT-small/ \
+#   --data_path ETTh1.csv \
+#   --model_id ETTh1_96_720 \
+#   --model Reformer \
+#   --data ETTh1 \
+#   --features M \
+#   --seq_len 96 \
+#   --label_len 48 \
+#   --pred_len 720 \
+#   --e_layers 2 \
+#   --d_layers 1 \
+#   --enc_in 7 \
+#   --dec_in 7 \
+#   --c_out 7 \
+#   --des 'Exp' \
+#   --itr 1 \
+#   --batch_size 30
 
 # 720
 python -u run.py \
@@ -44,4 +44,4 @@ python -u run.py \
   --c_out 7 \
   --des 'Exp' \
   --itr 1 \
-  --batch_size 30
+  --batch_size 16
